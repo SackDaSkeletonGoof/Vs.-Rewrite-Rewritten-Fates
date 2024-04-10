1797889fde2a662e327d83630ad18b6ff3aa81b2
Sack The Skeleton Goof <juliocmtza333@gmail.com>
2024-04-09 22:01:08 -0700

    im attempting to get a title screen going
    we will need a few more people for this

--- mods/Memorialize Prime/data/states/customStates/menus/rewritenTitle.hx
+++ mods/Memorialize Prime/data/states/customStates/menus/rewritenTitle.hx
@@ -1,5 +1,5 @@
 import funkin.backend.system.framerate.Framerate;
-var canDoShit:Bool = true;
+var transitioning, canPressEnter:Bool = false;
 Framerate.fpsCounter.visible = false;
 Framerate.memoryCounter.visible = false;
 Framerate.codenameBuildField.visible = false;
@@ -9,8 +9,8 @@ function new() CoolUtil.playMenuSong();
 static var initialized:Bool = false;
 
 var blackScreen:FlxSprite;
-var logoBl:FlxSprite;
-var ngSpr:FlxSprite;
+var thing:FlxSprite;
+var rewr:FlxSprite;
 var code:Int = 0;
 var wackyImage:FlxSprite;
 
@@ -30,13 +30,22 @@ function startIntro()
 		{
 
 
-			FlxG.sound.playMusic(Paths.music('hills'), 0);
+			FlxG.sound.playMusic(Paths.music('music/hills'), 0);
 
 			FlxG.sound.music.fadeIn(5, 0, 0.7);
 		}
 
 		Conductor.changeBPM(60);
 		persistentUpdate = true;
+
+		thing = new FlxSprite();
+		thing.loadGraphic(Paths.image('menus/titlepog'));
+    	thing.scale.x = 1;
+    	thing.scale.y = 1;
+		thing.screenCenter(FlxAxes.X);
+		thing.y = 100;
+    	thing.scrollFactor.set(0.4, 0.4);
+    	add(thing);
 	}
 
 
@@ -48,6 +57,12 @@ new FlxTimer().start(1, function(tmr:FlxTimer)
 		});
 }
 var transitioning:Bool = false;
+
+var weewoo:Float = 0;
 function update(elapsed:Float)
 	{
-    } 
+		weewoo += 0.03;
+
+		thing.y += Math.sin(weewoo) * 0.8;
+		thing.angle += Math.cos(weewoo) * 0.1;
+    }
