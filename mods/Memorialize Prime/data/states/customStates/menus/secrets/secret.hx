e82a6fa8ccaf6124f8c0f9eb44ecb3aa87bab109
Sack The Skeleton Goof <juliocmtza333@gmail.com>
2024-04-11 22:49:25 -0700

    pipis

--- mods/Memorialize Prime/data/states/customStates/menus/secrets/secret.hx
+++ mods/Memorialize Prime/data/states/customStates/menus/secrets/secret.hx
@@ -9,7 +9,7 @@ function create(){
     importScript("data/scripts/cool VHS");
     trace("if any one sees this. dw bout it, i was just messing around with the mod");
 
-    var textShit = new FlxText(0, 200, FlxG.width, "there is nothing here.", 19, true);
+    var textShit = new FlxText(0, 200, FlxG.width, "there is nothing here.\n\nPress ESC to get back to the title screen :)", 19, true);
 	textShit.setFormat("fonts/sonic.ttf", 60, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 	textShit.scrollFactor.set();
 	textShit.screenCenter(FlxAxes.X);
