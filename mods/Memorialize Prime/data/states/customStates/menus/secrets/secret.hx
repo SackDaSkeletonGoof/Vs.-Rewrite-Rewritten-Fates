3898154e9ccaa79a3f985b49a4326c069f7d4ac4
Sack The Skeleton Goof <juliocmtza333@gmail.com>
2024-04-11 22:41:07 -0700

    MORE UPDATES GRAAA
    YO DUDE- INAKURO- DO THE THING U WHERE GONNA DO BRUH- :sob:

--- mods/Memorialize Prime/data/states/customStates/menus/secrets/secret.hx
+++ mods/Memorialize Prime/data/states/customStates/menus/secrets/secret.hx
@@ -1,14 +1,28 @@
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import flixel.text.FlxTextBorderStyle;
+
+var textShit:FlxText;
+var thing:FlxSprite;
 
 function create(){
+    importScript("data/scripts/cool VHS");
     trace("if any one sees this. dw bout it, i was just messing around with the mod");
 
-    var g = new FlxText(0, 200, FlxG.width, "there is nothing here.", 19, true);
+    var textShit = new FlxText(0, 200, FlxG.width, "there is nothing here.", 19, true);
 	textShit.setFormat("fonts/sonic.ttf", 60, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 	textShit.scrollFactor.set();
 	textShit.screenCenter(FlxAxes.X);
-	textGroup.add(textShit);
+	add(textShit);
+
+    thing = new FlxSprite();
+	thing.loadGraphic(Paths.image('menus/rewr'));
+    thing.scale.x = 0.5;
+    thing.scale.y = 0.5;
+	thing.screenCenter(FlxAxes.X);
+	thing.y = 200;
+    thing.scrollFactor.set();
+	add(thing);
 }
 
 function update(){
