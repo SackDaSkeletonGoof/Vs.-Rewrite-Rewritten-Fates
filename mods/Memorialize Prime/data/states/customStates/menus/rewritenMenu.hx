bbcfe2f0fbc29951441d54bb987d3ec9e9e2543e
Sack The Skeleton Goof <juliocmtza333@gmail.com>
2024-04-11 22:45:48 -0700

    Inakuro, pls help :c

--- mods/Memorialize Prime/data/states/customStates/menus/rewritenMenu.hx
+++ mods/Memorialize Prime/data/states/customStates/menus/rewritenMenu.hx
@@ -1,4 +1,7 @@
 import funkin.editors.EditorPicker;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.text.FlxTextBorderStyle;
 
 var thing:FlxSprite;
 
@@ -9,12 +12,18 @@ function create(){
 
     thing = new FlxSprite();
 	thing.loadGraphic(Paths.image('menus/rewr'));
-    thing.scale.x = 1;
-    thing.scale.y = 1;
+    thing.scale.x = 0.5;
+    thing.scale.y = 0.5;
 	thing.screenCenter(FlxAxes.X);
-	thing.screenCenter(FlxAxes.Y);
+	thing.y = 200;
     thing.scrollFactor.set(0, 0);
 	add(thing);
+
+    var textShit = new FlxText(0, 200, FlxG.width, "For the time being, please press F to enter Freeplay\n\nInakuro aint doing the thing he said he would do so uhh- yeah :) - Sack", 19, true);
+	textShit.setFormat("fonts/sonic.ttf", 30, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+	textShit.scrollFactor.set();
+	textShit.screenCenter(FlxAxes.X);
+	add(textShit);
 }
 
 function update(){
