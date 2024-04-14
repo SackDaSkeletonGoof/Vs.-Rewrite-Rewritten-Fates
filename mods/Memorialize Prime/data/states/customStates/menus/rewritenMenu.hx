3987902992a0083f6bfa02a05b0e51c10a6a30af
Inakuro-Ashigawa <99925622+Inakuro-Ashigawa@users.noreply.github.com>
2024-04-13 19:15:05 -0500

    WIP main menu

--- mods/Memorialize Prime/data/states/customStates/menus/rewritenMenu.hx
+++ mods/Memorialize Prime/data/states/customStates/menus/rewritenMenu.hx
@@ -1,37 +1,54 @@
 import funkin.editors.EditorPicker;
+import funkin.options.OptionsMenu;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.text.FlxTextBorderStyle;
-
+var menuItems:FlxTypedGroup<FlxSprite>;
+var menuItems = new FlxTypedGroup();
+var optionShit:Array<String> = ['freeplay', 'credits'];
+var curSelected:Int = 0;
+public var canAccessDebugMenus:Bool = true;
 var thing:FlxSprite;
+var xval:Int = 200;
 
 function create(){
     trace("ok so, we changed into the main menu, this shit aint good enough");
 
-    importScript("data/scripts/cool VHS");
+    //importScript("data/scripts/cool VHS");
 
-    thing = new FlxSprite();
-	thing.loadGraphic(Paths.image('menus/rewr'));
-    thing.scale.x = 0.5;
-    thing.scale.y = 0.5;
-	thing.screenCenter(FlxAxes.X);
-	thing.y = 200;
-    thing.scrollFactor.set(0, 0);
-	add(thing);
 
-    var textShit = new FlxText(0, 200, FlxG.width, "For the time being, please press F to enter Freeplay\n\nInakuro aint doing the thing he said he would do so uhh- yeah :) - Sack", 19, true);
-	textShit.setFormat("fonts/sonic.ttf", 30, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-	textShit.scrollFactor.set();
-	textShit.screenCenter(FlxAxes.X);
-	add(textShit);
+    add(menuItems);
+    for (i in 0...optionShit.length)
+    {
+    var tex = Paths.getSparrowAtlas('menus/main_menu');   
+    var menuItem:FlxSprite = new FlxSprite(xval, 40 + (i * 340));			
+    if (i % 2 == 0) menuItem.x -= -200 + i * 400;
+
+    menuItem.frames = tex;
+    menuItem.animation.addByPrefix('idle', "main_menu " + optionShit[i], 24);
+    menuItem.animation.play('idle');
+    menuItem.ID = i;
+    menuItems.add(menuItem);
+    menuItem.scrollFactor.set();
+    menuItem.antialiasing = true;
+    }
 }
+
 
 function update(){
     if (FlxG.keys.justPressed.ESCAPE)
         {
             FlxG.switchState(new TitleState());
         }
+    if (controls.UP_P)
+        {
+            changeItem(-1);
+        }
 
+        if (controls.DOWN_P)
+        {
+           changeItem(1);
+        }
     if (FlxG.keys.justPressed.F){
         FlxG.switchState(new FreeplayState());
     }
@@ -40,4 +57,86 @@ function update(){
 		persistentUpdate = false;
 		openSubState(new EditorPicker());
 	}
-}
+    if (controls.ACCEPT)
+        {
+
+            {
+                selectedSomethin = true;
+                FlxG.sound.play(Paths.sound('confirmMenu'));
+                
+                if (FlxG.save.data.flashing)
+                    FlxFlicker.flicker(bgdesat, 1.1, 0.15, false);
+
+                menuItems.forEach(function(spr:FlxSprite)
+                {
+                    if (curSelected != spr.ID)
+                    {
+                        FlxTween.tween(spr, {alpha: 0}, .3, {
+                            ease: FlxEase.expoOut,
+                            onComplete: function(twn:FlxTween)
+                            {
+                                spr.kill();
+                            }
+                        });
+                    }
+                    else
+                    {
+        
+                        FlxTween.tween(FlxG.camera, {zoom: 1.1}, 2, {ease: FlxEase.expoOut});
+                        if (FlxG.save.data.flashing)
+                        {
+                            FlxFlicker.flicker(spr, 1, 0.06, false, false, function(flick:FlxFlicker)
+                            {
+                                goToState();
+                            });	
+                        }
+                        else
+                        {
+                            new FlxTimer().start(1, function(tmr:FlxTimer)
+                            {
+                                goToState();
+                            });
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+function changeItem(huh:Int = 0)
+	{
+		
+			curSelected += huh;
+
+			if (curSelected >= menuItems.length)
+				curSelected = 0;
+			if (curSelected < 0)
+				curSelected = menuItems.length - 1;
+
+		menuItems.forEach(function(spr:FlxSprite)
+		{
+			spr.animation.play('idle');
+			if (spr.ID == curSelected )
+			{
+				spr.x + 2;
+			}
+
+			spr.updateHitbox();
+		});
+	}
+
+function goToState()
+	{
+		var daChoice:String = optionShit[curSelected];
+		switch (daChoice)
+		{
+			case 'freeplay':
+				FlxG.switchState(new FreeplayState());
+				trace("Freeplay Menu Selected");
+
+			case 'credits':
+				FlxG.switchState(new OptionsMenu());
+				trace("going to da options");
+
+		}
+	}
