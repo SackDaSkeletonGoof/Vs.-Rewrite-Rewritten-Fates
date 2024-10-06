ad1dfdf21a4f2a8bf0bd84aa83a6e05a7a65d7b1
SuperSpeed2 <120737518+SuperSpeed2@users.noreply.github.com>
2024-10-06 00:28:43 -0500

    Brought back TitleState

--- mods/Memorialize Prime/data/global.hx
+++ mods/Memorialize Prime/data/global.hx
@@ -4,10 +4,11 @@ import sys.io.File;
 
 static var redirectStates:Map<FlxState, String> = [
     MainMenuState => "menus/rewriteMainMenuState",
+    TitleState => "customStates/menus/rewritenTitle",
 ];
 
 function preStateSwitch() {
-    window.title = "VS Rewrite: Rewritten Files";
+    window.title = "VS Rewrite: Rewritten Fates";
     //window.setIcon(Image.fromBytes(File.getBytes('mods/' + ModsFolder.currentModFolder + '/images/icon.png')));
 
     for (redirectState in redirectStates.keys())
